f98c9604b7e75c2f8d6701e7a3b259a58c64db9d
Hasiniaina0 <hasiniaina.faramalala@gmail.com>
2024-04-15 11:47:26 +0200

    update ConnexionHebergeur et locataire

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,28 +11,6 @@ import {
 
 export default function HomeScreen({ navigation }) {
   return (
-<<<<<<< HEAD
-    <ImageBackground
-      source={require("../assets/background.png")}
-      style={styles.backgroundImage}
-    >
-      <View style={styles.container}>
-        <Image source={require("../assets/logo.png")} style={styles.logo} />
-        <Text style={styles.slogan}>L'accord parfait entre jeunes et </Text>
-        <Text style={styles.slogan}>sages dans une colocation pleine</Text>
-        <Text style={styles.slogan}>de vie et de partage</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("Connexion")}
-        >
-          <Text style={styles.buttonText}>Se connecter</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.signUpButton}>
-          <Text style={styles.signUpButtonText}>Pas encore de compte ?</Text>
-        </TouchableOpacity>
-      </View>
-    </ImageBackground>
-=======
     <ImageBackground source={require('../assets/background.png')} style={styles.backgroundImage}>
     <View style={styles.container}>
       <Image source={require('../assets/logo.png')} style={styles.logo} />
@@ -47,7 +25,6 @@ export default function HomeScreen({ navigation }) {
       </TouchableOpacity>
     </View>
   </ImageBackground>
->>>>>>> ae954a297fb43ad4ff67f0d4f79045d240e543fb
   );
 }
 
