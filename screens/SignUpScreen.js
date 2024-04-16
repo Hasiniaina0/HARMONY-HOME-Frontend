50511f48586e09b160f49ba01c9bfa26ace2ad54
Al-Khansae <160146958+Al-Khansae@users.noreply.github.com>
2024-04-16 11:46:34 +0200

    maj taille slogan

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -58,7 +58,7 @@ const styles = StyleSheet.create({
     height: 320,
   },
   slogan: {
-    fontSize: 20,
+    fontSize: 16,
     color: "white",
     alignItems: "center",
     justifyContent: "center",
