735786cadf0409abe83958b8aa143c4ebf7b99be
Noëmie Louis-Joseph <noemie.louisjoseph1@gmail.com>
2024-04-25 09:43:47 +0200

    ras

--- screens/HebergeurProfilScreen.js
+++ screens/HebergeurProfilScreen.js
@@ -58,7 +58,6 @@ export default function HebergeurProfilScreen() {
         description,
         aPropos,
         photoProfil,
-        photoProfil,
         available: availability,
       }),
     })
